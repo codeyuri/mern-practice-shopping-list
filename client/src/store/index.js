10dab5b053776d77d909f385bd880900fadbd2db
Dan <danquesadaiii@gmail.com>
2020-05-05 01:57:45 +0800

    modified mobile devtools thunk

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,23 +1,12 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-// import { createStore, applyMiddleware } from 'redux';
 import Thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 
-// const store = createStore(
-//     rootReducer,
-//     applyMiddleware(Thunk)
-// )
-
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
+const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null
 
 const store = createStore(
   rootReducer,
-  compose(applyMiddleware(thunk), devTools)
+  compose(applyMiddleware(Thunk), devTools)
 )
-
-// const store = createStore(
-//     rootReducer,
-//     compose( applyMiddleware(Thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() )
-// )
 
 export default store;

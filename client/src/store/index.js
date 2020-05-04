b480a694a505c1b3b6e1e3bb3ea6c25bb9117d32
Dan <danquesadaiii@gmail.com>
2020-05-05 02:03:05 +0800

    modified mobile devtools 3

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import Thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
 
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null
+const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
 
 const store = createStore(
   rootReducer,
